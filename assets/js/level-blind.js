351ab442aa09d6100e0d664288bd5e6e73b24299
Bas de Bruin <bruin998@gmail.com>
2020-12-04 12:28:30 +0100

    looked into swipe detection, doesnt work yet

--- assets/js/level-blind.js
+++ assets/js/level-blind.js
@@ -9,6 +9,8 @@
 var blind = {
     // Vars
     $content: $('#content'),
+    // swipe detector
+    //hammer: new Hammer($('#blind-background')[0]),
 
     // Methods
     // Init
@@ -19,12 +21,25 @@ var blind = {
             $(elem).focus(() => blind.onFocus(elem));
 
         });
+
+        // TODO: detect swipes on mobile
+        // for now:
+        if ($(window).width() < 400) {
+            if (confirm("Dit level werkt nog niet op mobiel, omdat een toetsenbord nodig is. \nWil je dit level overslaan?")) {
+                showScore('100');
+            }
+        }
     },
 
     // Handle focus
     onFocus(elem) {
         console.log('focus');
         this.speak($(elem).html());
+    },
+
+    onSwipe(event) {
+        console.log('swipe', event);
+        $('#blind-background').css('background', 'red');
     },
 
     // Speaks string using SpeechSynthesis
@@ -38,4 +53,5 @@ blind.init();
 
 function unload() {
     delete blind;
+    delete Hammer;
 }
